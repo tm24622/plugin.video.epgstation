175c182a8f4eafe90f3a65f01fca77fe71c07f60
tm24622 <tky7170@kcn.jp>
2024-06-22 22:45:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def get_categories():
     return VIDEOS
 
 def get_categoryname(category):
-    names = [x['searchOption']['channel'] for x in rules if x['id'] == category]
-    return names[0] if len(names) else u'ルールなし'
+    names = [x['searchOption']['keyword'] for x in rules if x['id'] == category]
+    return names[1] if len(names) else u'ルールなし'
 
 def get_videos(category):
     return VIDEOS.get(category)
@@ -159,8 +159,8 @@ if __name__ == '__main__':
     VIDEOS={}
     for video in videos:
         # フォルダ分けにルール名を使用するため、ルールの名称を取得
-        rulenames = [x['searchOption']['channel'] for x in rules if x['id'] == video.get('ruleId')]
-        rulename = rulenames[0] if len(rulenames) else u'ルールなし'
+        rulenames = [x['searchOption']['keyword'] for x in rules if x['id'] == video.get('ruleId')]
+        rulename = rulenames[1] if len(rulenames) else u'ルールなし'
         
         thumbnail_url = urljoin(server_url, 'api/thumbnails/' + str(video['thumbnails'][0])) if len(video['thumbnails']) else ''
         for vf in video['videoFiles']:
